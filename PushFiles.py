4f7870b1eafdefce0296d748a1017151aafd83d6
Roger Pease <rogerpease@gmail.com>
2021-12-07 06:53:43 -0600

    Initial Revision

--- PushFiles.py
+++ PushFiles.py
@@ -2,10 +2,11 @@
 import os 
 
 MYPYNQBOARD="xilinx@192.168.1.128"
+PROJECTNAME="AXIMasterStreamTutorial"
 
 
 
 os.system("ssh "+MYPYNQBOARD+" mkdir /home/xilinx/AXIMasterStreamTutorial")
-os.system("scp ./FPGAImageProject/FPGAImageProject.gen/sources_1/bd/FPGABlockDesign/hw_handoff/FPGABlockDesign.hwh "+MYPYNQBOARD+":/home/xilinx/AXIMasterStreamTutorial/AXIMasterStreamTutorial.hwh")
-os.system("scp ./FPGAImageProject/FPGAImageProject.runs/impl_1/FPGABlockDesign_wrapper.bit "+MYPYNQBOARD+":/home/xilinx/AXIMasterStreamTutorial/AXIMasterStreamTutorial.bit")
-os.system("scp RecvStream.py  "+MYPYNQBOARD+":/home/xilinx/AXIMasterStreamTutorial/RecvStream.py")
+os.system("scp ./FPGAImageProject/FPGAImageProject.gen/sources_1/bd/FPGABlockDesign/hw_handoff/FPGABlockDesign.hwh "+MYPYNQBOARD+":/home/xilinx/"+PROJECTNAME+"/"+PROJECTNAME+".hwh")
+os.system("scp ./FPGAImageProject/FPGAImageProject.runs/impl_1/FPGABlockDesign_wrapper.bit "+MYPYNQBOARD+":/home/xilinx/"+PROJECTNAME+"/"+PROJECTNAME+".bit")
+os.system("scp RecvStream.py  "+MYPYNQBOARD+":/home/xilinx/"+PROJECTNAME+"/RecvStream.py")
---
+++ RecvStream.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3 
+
+from pynq import Overlay
+from pynq import allocate
+
+def compareLists(l1, l2):
+   if len(l1) != len(l2):
+      return False
+   return sorted(l1)==sorted(l2)
+
+
+def CaptureFramesAndTest(overlay):
+  output_buffer    = allocate(shape=(8,),dtype='u4')
+  output_buffer[:] = 0
+  overlay.axi_dma_0.recvchannel.start()
+  overlay.AXIMasterStreamTutor_0.write(0x4,0xDECADE00)
+  # Start Transfer then immediately issue stop. When Burst ends the State Machine will stop. 
+  overlay.AXIMasterStreamTutor_0.write(0x0,1)
+  overlay.AXIMasterStreamTutor_0.write(0x0,0)
+  # Capture Last Data Bus 
+  overlay.axi_dma_0.recvchannel.transfer(output_buffer)
+  goldenBuffer1 = [0xdecade00+x for x in range(1,9)]
+  if not (compareLists(output_buffer,goldenBuffer1)):
+      print ("Compare 1 FAILED")
+      for item in output_buffer:
+           print(hex(item))
+      for item in goldenBuffer1:
+           print(hex(item))
+      exit(1); 
+  else: 
+     print ("PASS 1"); 
+   
+  output_buffer[:] = 0 
+  overlay.AXIMasterStreamTutor_0.write(0x4,0xBEEF0000)
+  overlay.AXIMasterStreamTutor_0.write(0x0,1) # Start and set to stop. 
+  overlay.AXIMasterStreamTutor_0.write(0x0,0)
+  overlay.axi_dma_0.recvchannel.transfer(output_buffer)
+  goldenBuffer2 = [0xBEEF0000+x for x in range(1,9)]
+  if not (compareLists(output_buffer,goldenBuffer2)):
+      print ("Compare 2 FAILED")
+      exit(1); 
+  else: 
+     print ("PASS 2"); 
+
+  overlay.axi_dma_0.recvchannel.stop() 
+  
+
+overlay = Overlay("AXIMasterStreamTutorial.bit")
+CaptureFramesAndTest(overlay)
